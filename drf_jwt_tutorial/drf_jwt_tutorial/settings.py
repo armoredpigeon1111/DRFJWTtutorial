921279891b12a8518b6ee271812b6f520af96297
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-10-06 14:21:35 -0400

    Created Registration Serializer Class + Created Register View Class for Authentication App

--- drf_jwt_tutorial/drf_jwt_tutorial/settings.py
+++ drf_jwt_tutorial/drf_jwt_tutorial/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +116,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
 
 try:
     from drf_jwt_tutorial.local_settings import *
