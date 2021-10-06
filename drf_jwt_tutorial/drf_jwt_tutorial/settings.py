da77189d032523140f17efe660db040bc05aeb94
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-10-06 13:57:17 -0400

    Created Database + Setup Database Settings files for project

--- drf_jwt_tutorial/drf_jwt_tutorial/settings.py
+++ drf_jwt_tutorial/drf_jwt_tutorial/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1re6nn_^!#y=i#fb-56jquu)8rp&-bm)1rixnz8%5g2%(n)^qb'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +69,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'drf_jwt_tutorial.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -123,3 +114,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from drf_jwt_tutorial.local_settings import *
+except ImportError:
+    pass
